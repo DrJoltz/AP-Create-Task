1c37d0c147453af0449388a41274d77d9265c38c
Jeremy Brien <joltzdr@gmail.com>
2018-03-15 12:28:50 -0400

    Initial Commit

---
+++ Create Task v3/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Create Task v3
+//
+//  Created by Jeremy Brien on 3/15/18.
+//  Copyright © 2018 Jeremy Brien. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
